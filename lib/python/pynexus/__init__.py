a002d6c41eb2cccdbe96c8f2f2499b8c3ef64baf
Mustafa Ozuysal <mustafa.oezuysal@gmail.com>
2013-07-23 10:43:50 +0300

    fix python package file names

---
+++ lib/python/pynexus/__init__.py
@@ -0,0 +1,16 @@
+import ctypes as _C
+
+_nexus = _C.cdll.LoadLibrary("libvirg-nexus.so")
+
+Bool = _C.c_int
+Result = _C.c_int
+uchar = _C.c_ubyte
+
+(FALSE, TRUE) = (0, 1)
+EXIT_FATAL = -99
+OK = 0
+FAIL = -1
+SIMD_ALIGNMENT = 32
+LOG_TAG = "VIRG-NEXUS"
+
+__all__ = ["Bool", "Result", "uchar", "FALSE", "TRUE", "EXIT_FATAL", "OK", "FAIL", "SIMD_ALINGMENT", "LOG_TAG"]
---
+++ lib/python/pynexus/wrappers/nximage.py
@@ -0,0 +1,121 @@
+import ctypes as _C
+
+import pynexus.image as _Img
+
+__all__ = [ "NXImage" ]
+
+_POINTER = _C.POINTER
+_c_ubyte = _C.c_ubyte
+_c_int = _C.c_int
+_c_size_t = _C.c_size_t
+_c_float = _C.c_float
+_c_char_p = _C.c_char_p
+
+class NXImage:
+    """Wrapper class for directly working with VIRG-Nexus images"""
+    def __init__(self, w = None, h = None, image_type = _Img.GRAYSCALE):
+        self.__ptr = _Img.alloc()
+        if w and h:
+            self.resize(w, h, image_type)
+
+    def __del__(self):
+        _Img.free(self.__ptr)
+
+    def __str__(self):
+        return "<NXImage: {}x{}x{}>".format(self.width, self.height, self.n_channels);
+
+    @property
+    def ptr(self):
+        return self.__ptr;
+
+    @property
+    def width(self):
+        return self.__ptr.contents.width;
+
+    @property
+    def height(self):
+        return self.__ptr.contents.height;
+
+    @property
+    def type(self):
+        return self.__ptr.contents.type;
+
+    @property
+    def row_stride(self):
+        return self.__ptr.contents.row_stride;
+
+    @property
+    def n_channels(self):
+        return self.__ptr.contents.n_channels;
+
+    def get_pixel(self, x, y):
+        w = self.width
+        h = self.height
+        stride = self.row_stride
+        if (x < 0) or (x >= w) or (y < 0) or (y >= h):
+            return None
+        else:
+            return self.__ptr.contents.data[stride*y+x]
+
+    def set_pixel(self, x, y, value):
+        w = self.width
+        h = self.height
+        stride = self.row_stride
+        if (x < 0) or (x >= w) or (y < 0) or (y >= h):
+            return
+
+        value = _NX.uchar(value)
+        self.__ptr.contents.data[stride*y+x] = value
+
+    def resize(self, w, h, image_type):
+        _Img.resize(self.__ptr, w, h, NXImage.n_channels_by_type(image_type)*w, image_type)
+
+    def xsave_pnm(self, filename):
+        _Img.xsave_pnm(self.__ptr, filename)
+
+    def xload_pnm(self, filename, load_mode):
+        _Img.xload_pnm(self.__ptr, filename, load_mode)
+
+    def copy(self):
+        img = NXImage()
+        _Img.copy(img.__ptr, self.__ptr)
+        return img
+
+    def swap(self, img):
+        _Img.swap(img.__ptr, self.__ptr)
+
+    def set_zero(self):
+        _Img.set_zero(self.__ptr)
+
+    def scale(self, scale_factor):
+        img = NXImage()
+        sf = _c_float(scale_factor)
+        _Img.scale(img.__ptr, self.__ptr, sf)
+        return img
+
+    def downsample(self):
+        img = NXImage()
+        _Img.downsample(img.__ptr, self.__ptr)
+        return img
+
+    def smooth(self, sigma_x, sigma_y):
+        img = NXImage()
+        sx = _c_float(sigma_x)
+        sy = _c_float(sigma_y)
+        buffer = _POINTER(_NX.uchar)() # NULL pointer
+        _Img.smooth_s(img.__ptr, self.__ptr, sx, sy, buffer)
+        return img
+
+    def convert(self, image_type):
+        img = NXImage(1, 1, image_type)
+        _Img.convert(img.__ptr, self.__ptr)
+        return img
+
+    @staticmethod
+    def n_channels_by_type(image_type):
+        if image_type == _Img.GRAYSCALE:
+            return 1
+        elif image_type == _Img.RGBA:
+            return 4
+        else:
+            return 0
