f641ea318b0a9863138c449a99a5dfea7e0fbbf2
Mustafa Ozuysal <mustafa.oezuysal@gmail.com>
2013-07-27 00:19:27 +0300

    Fast detector complete/not tested

--- src/nx_fast_detector.c
+++ src/nx_fast_detector.c
@@ -13,6 +13,11 @@
 #include "virg/nexus/nx_fast_detector.h"
 
 #include "virg/nexus/nx_assert.h"
+#include "virg/nexus/nx_alloc.h"
+#include "virg/nexus/nx_math.h"
+#include "virg/nexus/nx_mem_block.h"
+
+#define NX_FAST_DETECTOR_WORK_MULTIPLIER 5
 
 extern void fast9_detect(struct NXKeypoint *ret_corners, const unsigned char *im, int xsize, int ysize, int stride, int b, int *ret_num_corners);
 extern void fast9_score (const unsigned char *i, int stride, struct NXKeypoint *corners, int num_corners, int b);
@@ -26,11 +31,20 @@ int nx_fast_detect_keypoints(int n_keys_max, struct NXKeypoint *keys,
 
         fast9_detect(keys, img->data, img->width, img->height,
                      img->row_stride, threshold, &n_keys_max);
+
+        for (int i = 0; i < n_keys_max; ++i) {
+                keys[i].xs = keys[i].x;
+                keys[i].ys = keys[i].y;
+                keys[i].score = 0.0f;
+                keys[i].ori = 0.0f;
+                keys[i].id = i;
+        }
+
         return n_keys_max;
 }
 
 void nx_fast_score_keypoints(int n_keys, struct NXKeypoint *keys,
-                            const struct NXImage *img, int threshold)
+                             const struct NXImage *img, int threshold)
 {
         NX_ASSERT_PTR(keys);
         NX_ASSERT_PTR(img);
@@ -57,8 +71,10 @@ int nx_fast_detect_keypoints_pyr(int n_keys_supp_max, struct NXKeypoint *keys_su
 
                 n_level_keys = nx_fast_suppress_keypoints(n_level_keys_max, level_keys,
                                                           n_level_keys, keys);
-                // Fix keypoint ids
+                // Fill scales/sigmas, fix keypoint ids
                 for (int j = 0; j < n_level_keys; ++j) {
+                        level_keys[j].sigma = pyr->levels[i].sigma;
+                        level_keys[j].scale = pyr->levels[i].scale;
                         level_keys[j].id = n_keys_supp + j;
                 }
 
@@ -77,5 +93,113 @@ int nx_fast_suppress_keypoints(int n_keys_supp_max, struct NXKeypoint *keys_supp
         NX_ASSERT_PTR(keys_supp);
 
         fast_nonmax_suppression(&n_keys_supp_max, keys_supp, n_keys, keys);
+        for (int i = 0; i < n_keys_supp_max; ++i) {
+                keys_supp[i].id = i;
+        }
+
         return n_keys_supp_max;
+}
+
+struct NXFastDetector *nx_fast_detector_alloc()
+{
+        struct NXFastDetector *detector = nx_new(1, struct NXFastDetector);
+
+        detector->max_n_keys = 0;
+        detector->threshold = 255;
+
+        detector->n_keys = 0;
+        detector->keys = NULL;
+
+        detector->n_work = 0;
+        detector->keys_work = NULL;
+        detector->mem = nx_mem_block_alloc();
+
+        return detector;
+}
+
+struct NXFastDetector *nx_fast_detector_new(int max_n_keys, int initial_work_size)
+{
+        NX_ASSERT(max_n_keys > 0);
+        NX_ASSERT(initial_work_size >= max_n_keys);
+
+        struct NXFastDetector *detector = nx_new(1, struct NXFastDetector);
+        nx_fast_detector_resize(detector, max_n_keys, initial_work_size);
+        return detector;
+}
+
+void nx_fast_detector_free(struct NXFastDetector *detector)
+{
+        if (detector) {
+                nx_free(detector->keys);
+                nx_mem_block_free(detector->mem);
+                nx_free(detector);
+        }
+}
+
+void nx_fast_detector_resize(struct NXFastDetector *detector, int max_n_keys, int initial_work_size)
+{
+        NX_ASSERT_PTR(detector);
+        NX_ASSERT(max_n_keys > 0);
+        NX_ASSERT(initial_work_size >= max_n_keys);
+
+        detector->max_n_keys = max_n_keys;
+        detector->threshold = 15;
+
+        detector->n_keys = 0;
+        detector->keys = nx_new(max_n_keys, struct NXKeypoint);
+
+        detector->n_work = max_n_keys * NX_FAST_DETECTOR_WORK_MULTIPLIER;
+        nx_mem_block_resize(detector->mem, detector->n_work * sizeof(struct NXKeypoint));
+        detector->keys_work = (struct NXKeypoint *)detector->mem->ptr;
+}
+
+void nx_fast_detector_detect(struct NXFastDetector *detector, const struct NXImage *img)
+{
+        NX_ASSERT_PTR(detector);
+        NX_ASSERT_PTR(img);
+
+        int n_keys = nx_fast_detect_keypoints(detector->n_work,
+                                              detector->keys_work,
+                                              img, detector->threshold);
+
+        nx_fast_score_keypoints(n_keys, detector->keys_work,
+                                img, detector->threshold);
+
+
+        detector->n_keys = nx_fast_suppress_keypoints(detector->max_n_keys,
+                                                      detector->keys,
+                                                      n_keys, detector->keys_work);
+}
+
+void nx_fast_detector_detect_pyr(struct NXFastDetector *detector, const struct NXImagePyr *pyr)
+{
+        NX_ASSERT_PTR(detector);
+        NX_ASSERT_PTR(pyr);
+
+        detector->n_keys = nx_fast_detect_keypoints_pyr(detector->max_n_keys,
+                                                        detector->keys,
+                                                        detector->n_work,
+                                                        detector->keys_work,
+                                                        pyr,
+                                                        detector->threshold);
+}
+
+void nx_fast_detector_adapt_threshold(struct NXFastDetector *detector)
+{
+        NX_ASSERT_PTR(detector);
+
+        int lim_high      = (1014 * detector->max_n_keys) >> 10; // n > ~0.99*max
+        int lim_low       = ( 921 * detector->max_n_keys) >> 10; // n > ~0.90*max
+        int lim_very_low  = ( 716 * detector->max_n_keys) >> 10; // n > ~0.70*max
+
+        if( detector->n_keys >= lim_high ) {
+            detector->threshold = nx_min_i(250, detector->threshold + 3);
+        } else if( detector->n_keys <= lim_very_low ) {
+            detector->threshold = nx_max_i(5, detector->threshold - 3);
+        } else if( detector->n_keys <= lim_low ) {
+            detector->threshold = nx_max_i(5, detector->threshold - 1);
+        }
+
+        NX_ASSERT(detector->threshold >= 5);
+        NX_ASSERT(detector->threshold <= 250);
 }
