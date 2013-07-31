c237232c6b55ff34021e776100fc6b766532fb4b
Mustafa Ozuysal <mustafa.oezuysal@gmail.com>
2013-07-31 16:31:06 +0300

    SFMT random number generator

--- include/virg/nexus/nx_types.h
+++ include/virg/nexus/nx_types.h
@@ -18,6 +18,8 @@
 #ifdef VIRG_NEXUS_PC_WIN32
 typedef __int32 int32_t;
 typedef unsigned __int32 uint32_t;
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
 #else
 #  include <inttypes.h>
 #endif
