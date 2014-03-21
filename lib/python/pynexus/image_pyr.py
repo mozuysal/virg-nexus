7bb8d9f15b9dae5795be299909676d4cb2f61b94
Mustafa Ozuysal <mustafa.oezuysal@gmail.com>
2014-03-21 11:21:33 +0200

    image pyr and pyr builder Python wrappers

--- lib/python/pynexus/image_pyr.py
+++ lib/python/pynexus/image_pyr.py
@@ -10,45 +10,16 @@ _c_ubyte = _C.c_ubyte
 _c_int = _C.c_int
 _c_float = _C.c_float
 
-Type = _C.c_int
-
-(FAST, FINE, SCALED) = (0, 1, 2)
-
 class Level(_C.Structure):
     _fields_ = [('img', image.Ptr),
                 ('sigma', _c_float),
                 ('scale', _c_float)]
 
-class FastInfo(_C.Structure):
-    _fields_ = [('width0', _c_int),
-                ('height0', _c_int),
-                ('sigma0', _c_float)]
-
-class FineInfo(_C.Structure):
-    _fields_ = [('width0', _c_int),
-                ('height0', _c_int),
-                ('n_octaves', _c_int),
-                ('n_octave_steps', _c_int),
-                ('sigma0', _c_float)]
-
-class ScaledInfo(_C.Structure):
-    _fields_ = [('width0', _c_int),
-                ('height0', _c_int),
-                ('scale_factor', _c_float),
-                ('sigma0', _c_float)]
-
-class Info(_C.Union):
-    _fields_ = [('fast', FastInfo),
-                ('fine', FineInfo),
-                ('scaled', ScaledInfo)]
 
 
 class Struct(_C.Structure):
     _fields_ = [('n_levels', _c_int),
-                ('levels', _POINTER(Level)),
-                ('type', Type),
-                ('info', Info),
-                ('work_img', image.Ptr)]
+                ('levels', _POINTER(Level))]
 
     def __str__(self):
         return "<pynexus.image_pyr.Struct: {} Levels>".format(self.n_levels);
@@ -59,51 +30,22 @@ alloc = _nexus.nx_image_pyr_alloc
 alloc.restype = Ptr
 alloc.argtypes = []
 
-new_fast = _nexus.nx_image_pyr_new_fast
-new_fast.restype = Ptr
-new_fast.argtypes = [_c_int, _c_int, _c_int, _c_float]
-
-new_fine = _nexus.nx_image_pyr_new_fine
-new_fine.restype = Ptr
-new_fine.argtypes = [_c_int, _c_int, _c_int, _c_int, _c_float]
-
-new_scaled = _nexus.nx_image_pyr_new_scaled
-new_scaled.restype = Ptr
-new_scaled.argtypes = [_c_int, _c_int, _c_int, _c_float, _c_float]
-
 free = _nexus.nx_image_pyr_free
 free.restype = None
 free.argtypes = [Ptr]
 
-resize_fast = _nexus.nx_image_pyr_resize_fast
-resize_fast.restype = None
-resize_fast.argtypes = [Ptr, _c_int, _c_int, _c_int, _c_float]
+alloc_levels = _nexus.nx_image_pyr_alloc_levels
+alloc_levels.restype = None
+alloc_levels.argtypes = [Ptr, _c_int]
 
-resize_fine = _nexus.nx_image_pyr_resize_fine
-resize_fine.restype = None
-resize_fine.argtypes = [Ptr, _c_int, _c_int, _c_int, _c_int, _c_float]
+free_levels = _nexus.nx_image_pyr_free_levels
+free_levels.restype = None
+free_levels.argtypes = [Ptr]
 
-resize_scaled = _nexus.nx_image_pyr_resize_scaled
-resize_scaled.restype = None
-resize_scaled.argtypes = [Ptr, _c_int, _c_int, _c_int, _c_float, _c_float]
+create_level = _nexus.nx_image_pyr_create_level
+create_level.restype = None
+create_level.argtypes = [Ptr, _c_int, _c_int, _c_int, _c_float, _c_float]
 
-release = _nexus.nx_image_pyr_release
-release.restype = None
-release.argtypes = [Ptr]
-
-compute = _nexus.nx_image_pyr_compute
-compute.restype = None
-compute.argtypes = [Ptr, image.Ptr]
-
-update = _nexus.nx_image_pyr_update
-update.restype = None
-update.argtypes = [Ptr]
-
-copy0 = _nexus.nx_image_pyr_copy0
-copy0.restype = Ptr
-copy0.argtypes = [Ptr]
-
-copy = _nexus.nx_image_pyr_copy
-copy.restype = None
-copy.argtypes = [Ptr, Ptr]
-
+copy_to_level0 = _nexus.nx_image_pyr_copy_to_level0
+copy_to_level0.restype = None
+copy_to_level0.argtypes = [Ptr, image.Ptr, _c_float]
--- lib/python/pynexus/wrappers/nximagepyr.py
+++ lib/python/pynexus/wrappers/nximagepyr.py
@@ -5,7 +5,7 @@ import pynexus.image as _Img
 import pynexus.image_pyr as _Pyr
 from pynexus.wrappers.nximage import NXImage as _NXImg
 
-__all__ = [ "NXImagePyr", "NXImagePyrInfo", "NXImagePyrLevel" ]
+__all__ = [ "NXImagePyr", "NXImagePyrLevel" ]
 
 _POINTER = _C.POINTER
 _c_ubyte = _C.c_ubyte
@@ -13,33 +13,6 @@ _c_int = _C.c_int
 _c_size_t = _C.c_size_t
 _c_float = _C.c_float
 _c_char_p = _C.c_char_p
-
-class NXImagePyrInfo(object):
-    """Wrapper class for extracting information on VIRG-Nexus image pyramids"""
-    def __init__(self, pyr):
-        ptr = NXImagePyr.ptr_of(pyr)
-
-        if ptr.contents.type == NX_IMAGE_PYR_FAST:
-            inf = ptr.contents.info.fast
-            self.type = NX_IMAGE_PYR_FAST
-            self.width0 = inf.width0
-            self.height0 = inf.height0
-            self.sigma0 = inf.sigma0
-        elif ptr.contents.type == NX_IMAGE_PYR_FINE:
-            inf = ptr.contents.info.fine
-            self.type = NX_IMAGE_PYR_FINE
-            self.width0 = inf.width0
-            self.height0 = inf.height0
-            self.n_octaves = inf.n_octaves
-            self.n_octave_steps = inf.n_octave_steps
-            self.sigma0 = inf.sigma0
-        elif ptr.contents.type == NX_IMAGE_PYR_SCALED:
-            inf = ptr.contents.info.scaled
-            self.type = NX_IMAGE_PYR_SCALED
-            self.width0 = inf.width0
-            self.height0 = inf.height0
-            self.scale_factor = inf.scale_factor
-            self.sigma0 = inf.sigma0
 
 class NXImagePyrLevel(object):
     """Wrapper class for VIRG-Nexus image pyramid levels"""
@@ -89,10 +62,6 @@ class NXImagePyrLevel(object):
 class NXImagePyr(object):
     """Wrapper class for directly working with VIRG-Nexus image pyramids"""
 
-    FAST = _Pyr.FAST
-    FINE = _Pyr.FINE
-    SCALED = _Pyr.SCALED
-
     def __init__(self):
         self.__ptr = _Pyr.alloc()
 
@@ -123,62 +92,11 @@ class NXImagePyr(object):
     def levels(self):
         return self.__ptr.contents.levels
 
-    @property
-    def type(self):
-        return self.__ptr.contents.type
-
-    @property
-    def info(self):
-        return NXImagePyrInfo(self)
-
     def level(self, level_id):
         if (level_id >= 0) and (level_id < self.n_levels):
             return NXImagePyrLevel(self, level_id)
         else:
             raise IndexError('level: level index {} is out of bounds for {} level pyramid!'.format(level_id, self.n_levels))
-
-    def resize_fast(self, width, height, n_levels, sigma0 = None):
-        if sigma0 is None:
-            sigma0 = 0.0
-        _Pyr.resize_fast(self.__ptr, width, height, n_levels, sigma0)
-
-    def resize_fine(self, width, height, n_octaves, n_octave_steps, sigma0 = None):
-        if sigma0 is None:
-            sigma0 = 0.0
-        _Pyr.resize_fine(self.__ptr, width, height, n_octaves, n_octave_steps, sigma0)
-
-    def resize_scaled(self, width, height, n_levels, scale_f, sigma0 = None):
-        if sigma0 is None:
-            sigma0 = 0.0
-        _Pyr.resize_scaled(self.__ptr, width, height, n_levels, scale_f, sigma0)
-
-    def update(self):
-        _Pyr.update(self.__ptr)
-
-    def compute(self, img):
-        ptr = _NXImg.ptr_of(img)
-        if not (ptr.contents.type == _NXImg.GRAYSCALE): raise TypeError('Image must be grayscale!')
-        _Pyr.compute(self.__ptr, ptr)
-
-    def compute_fast(self, img, n_levels, sigma0 = None):
-        if not (_NXImg.ptr_of(img).contents.type == _NXImg.GRAYSCALE): raise TypeError('Image must be grayscale!')
-        self.resize_fast(img.width, img.height, n_levels, sigma0)
-        self.compute(img)
-
-    def compute_fine(self, img, n_octaves, n_octave_steps, sigma0 = None):
-        if not (_NXImg.ptr_of(img).contents.type == _NXImg.GRAYSCALE): raise TypeError('Image must be grayscale!')
-        self.resize_fine(img.width, img.height, n_octaves, n_octave_steps, sigma0)
-        self.compute(img)
-
-    def compute_scaled(self, img, n_levels, scale_f, sigma0 = None):
-        if not (_NXImg.ptr_of(img).contents.type == _NXImg.GRAYSCALE): raise TypeError('Image must be grayscale!')
-        self.resize_scaled(img.width, img.height, n_levels, scale_f, sigma0)
-        self.compute(img)
-
-    def copy(self):
-        pyr = NXImagePyr()
-        _Pyr.copy(pyr.__ptr, self.__ptr)
-        return pyr
 
     @staticmethod
     def ptr_of(pyr):
