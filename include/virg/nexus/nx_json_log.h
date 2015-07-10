53e6ac5928646070419e9a558ebd1477a124263f
Mustafa Ozuysal <mustafaozuysal@iyte.edu.tr>
2015-07-10 22:32:00 +0300

    add json logger

---
+++ include/virg/nexus/nx_json_log.h
@@ -0,0 +1,69 @@
+/**
+ * @file nx_json_log.h
+ *
+ * This file is part of the IYTE Visual Intelligence Research Group Software Library
+ *
+ * Copyright (C) 2015 Mustafa Ozuysal. All rights reserved.
+ *
+ * @author Mustafa Ozuysal
+ *
+ * Contact mustafaozuysal@iyte.edu.tr for comments and bug reports.
+ *
+ */
+#ifndef VIRG_NEXUS_NX_JSON_LOG_H
+#define VIRG_NEXUS_NX_JSON_LOG_H
+
+#include "virg/nexus/nx_config.h"
+#include "virg/nexus/nx_io.h"
+#include "virg/nexus/nx_json_tree.h"
+#include "virg/nexus/nx_json_bundle.h"
+
+__NX_BEGIN_DECL
+
+#if defined(NX_ENABLE_JLOG)
+#define NX_JLOG_N_STREAMS 32
+#define NX_JLOG_PRETTY_PRINT_LEVEL 1
+
+struct NXJSONNode *nx_json_log_get_stream(int jlog_id);
+void nx_json_log_free();
+
+#define NX_JLOG(jlogid,key,value) nx_json_object_add(nx_json_log_get_stream(jlogid), (key), (value))
+#define NX_JLOG_TEMPLATE(jlogid,key,value,jbundlefn) nx_json_object_add(nx_json_log_get_stream(jlogid), (key), jbundlefn(value))
+#define NX_JLOG_ARR_TEMPLATE(jlogid,key,n,value,jbundlefn) nx_json_object_add(nx_json_log_get_stream(jlogid), (key), jbundlefn((n),(value)))
+#define NX_JLOG_ADD_TEMPLATE(jlogid,key,value,jbundlefn) nx_json_array_add(nx_json_object_fget(nx_json_log_get_stream(jlogid), (key), NX_JSON_ARRAY), jbundlefn(value))
+#define NX_JLOG_PRINT(jlogid) nx_json_tree_print(nx_json_log_get_stream(jlogid), NX_JLOG_PRETTY_PRINT_LEVEL)
+#define NX_JLOG_FPRINT(jlogid,stream) nx_json_tree_fprint(stream,nx_json_log_get_stream(jlogid), NX_JLOG_PRETTY_PRINT_LEVEL)
+#define NX_JLOG_XWRITE(jlogid,filename)                                 \
+        do {                                                            \
+                FILE *nx_jlog_stream = nx_xfopen(filename,"w");         \
+                nx_json_tree_fprint(nx_jlog_stream, nx_json_log_get_stream(jlogid), NX_JLOG_PRETTY_PRINT_LEVEL); \
+                nx_xfclose(nx_jlog_stream);                             \
+        } while (0);
+#else
+#  define NX_JLOG(jlogid,key,value) do { (void)sizeof(value); } while(0)
+#  define NX_JLOG_TEMPLATE(jlogid,key,value,jbundlefn) do { (void)sizeof(value); } while(0)
+#  define NX_JLOG_ARR_TEMPLATE(jlogid,key,n,value,jbundlefn) do { (void)sizeof(value); } while(0)
+#  define NX_JLOG_ADD_TEMPLATE(jlogid,key,value,jbundlefn) do { (void)sizeof(value); } while(0)
+#  define NX_JLOG_PRINT(jlogid) do { (void)sizeof(jlogid); } while(0)
+#  define NX_JLOG_FPRINT(jlogid,stream) do { (void)sizeof(jlogid); } while(0)
+#  define NX_JLOG_XWRITE(jlogid,filename) do { (void)sizeof(jlogid); } while(0)
+#endif
+
+#define NX_JLOG_I(jlogid,key,value) NX_JLOG_TEMPLATE(jlogid,key,value,nx_json_bundle_int)
+#define NX_JLOG_F(jlogid,key,value) NX_JLOG_TEMPLATE(jlogid,key,value,nx_json_bundle_float)
+#define NX_JLOG_D(jlogid,key,value) NX_JLOG_TEMPLATE(jlogid,key,value,nx_json_bundle_double)
+#define NX_JLOG_S(jlogid,key,value) NX_JLOG_TEMPLATE(jlogid,key,value,nx_json_bundle_string)
+
+#define NX_JLOG_IA(jlogid,key,n,value) NX_JLOG_ARR_TEMPLATE(jlogid,key,n,value,nx_json_bundle_int_array)
+#define NX_JLOG_FA(jlogid,key,n,value) NX_JLOG_ARR_TEMPLATE(jlogid,key,n,value,nx_json_bundle_float_array)
+#define NX_JLOG_DA(jlogid,key,n,value) NX_JLOG_ARR_TEMPLATE(jlogid,key,n,value,nx_json_bundle_double_array)
+#define NX_JLOG_SA(jlogid,key,n,value) NX_JLOG_ARR_TEMPLATE(jlogid,key,n,value,nx_json_bundle_string_array)
+
+#define NX_JLOG_ADDI(jlogid,key,value) NX_JLOG_ADD_TEMPLATE(jlogid,key,value,nx_json_bundle_int)
+#define NX_JLOG_ADDF(jlogid,key,value) NX_JLOG_ADD_TEMPLATE(jlogid,key,value,nx_json_bundle_float)
+#define NX_JLOG_ADDD(jlogid,key,value) NX_JLOG_ADD_TEMPLATE(jlogid,key,value,nx_json_bundle_double)
+#define NX_JLOG_ADDS(jlogid,key,value) NX_JLOG_ADD_TEMPLATE(jlogid,key,value,nx_json_bundle_string)
+
+__NX_END_DECL
+
+#endif
---
+++ src/nx_json_log.c
@@ -0,0 +1,46 @@
+/**
+ * @file nx_json_log.c
+ *
+ * This file is part of the IYTE Visual Intelligence Research Group Software Library
+ *
+ * Copyright (C) 2015 Mustafa Ozuysal. All rights reserved.
+ *
+ * @author Mustafa Ozuysal
+ *
+ * Contact mustafaozuysal@iyte.edu.tr for comments and bug reports.
+ *
+ */
+#define NX_ENABLE_JLOG 1
+#include "virg/nexus/nx_json_log.h"
+
+#include "virg/nexus/nx_alloc.h"
+#include "virg/nexus/nx_message.h"
+
+static struct NXJSONNode **nx_s_json_log = NULL;
+
+struct NXJSONNode *nx_json_log_get_stream(int jlog_id)
+{
+        if (nx_s_json_log == NULL) {
+                nx_s_json_log = NX_NEW(NX_JLOG_N_STREAMS, struct NXJSONNode *);
+                for (int i = 0; i < NX_JLOG_N_STREAMS; ++i)
+                        nx_s_json_log[i] = NULL;
+        }
+
+        if (jlog_id < 0 || jlog_id >= NX_JLOG_N_STREAMS)
+                nx_fatal(NX_LOG_TAG, "JSON log id %d is not in range [0, %d]",
+                         jlog_id, NX_JLOG_N_STREAMS-1);
+        else if (nx_s_json_log[jlog_id] == NULL)
+                nx_s_json_log[jlog_id] = nx_json_node_new_object();
+
+        return nx_s_json_log[jlog_id];
+}
+
+void nx_json_log_free()
+{
+        if (nx_s_json_log != NULL) {
+                for (int i = 0; i < NX_JLOG_N_STREAMS; ++i)
+                        nx_json_tree_free(nx_s_json_log[i]);
+                nx_free(nx_s_json_log);
+                nx_s_json_log = NULL;
+        }
+}
