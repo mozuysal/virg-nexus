d13ff026cc846599bdec066d523d968bc7a53142
Mustafa Ozuysal <mustafaozuysal@iyte.edu.tr>
2015-11-29 22:21:56 +0200

    implement JSON lexer in terms of lexer and add unit tests

--- src/nx_json_lexer.c
+++ src/nx_json_lexer.c
@@ -20,14 +20,15 @@
 #include "virg/nexus/nx_alloc.h"
 #include "virg/nexus/nx_string.h"
 #include "virg/nexus/nx_message.h"
-
-#define JSON_TAB_WIDTH 8
+#include "virg/nexus/nx_lexer.h"
 
 #define NX_JSON_LEXER_ERROR_LINE_COL_INFO "Line %d, Column %d: "
 #define NX_JSON_LEXER_ERROR(lx,msg,...)                                 \
         do {                                                            \
                 fflush(stdout);                                         \
-                nx_fatal("NXJSONLexer",NX_JSON_LEXER_ERROR_LINE_COL_INFO msg,(lx)->line_no,(lx)->col_no,##__VA_ARGS__); \
+                int line_no = nx_lexer_line_no((lx)->lex);              \
+                int col_no = nx_lexer_column_no((lx)->lex);             \
+                nx_fatal("NXJSONLexer",NX_JSON_LEXER_ERROR_LINE_COL_INFO msg,line_no,col_no,##__VA_ARGS__); \
         } while (0)
 
 static char *g_json_token_names[] = {
@@ -111,49 +112,20 @@ const char *nx_json_token_type_name(enum NXJSONTokenType type)
 
 struct NXJSONLexer
 {
-        char *s;
-        int p;
-        int c;
-
-        int line_no;
-        int col_no;
+        struct NXLexer *lex;
 };
-
-static void nx_json_lexer_consume(struct NXJSONLexer *jlex)
-{
-        jlex->c = jlex->s[++jlex->p];
-        if (jlex->c == '\0')
-                jlex->c = EOF;
-
-        if (jlex->c == '\n') {
-                jlex->line_no++;
-                jlex->col_no = 0;
-        } else if (jlex->c == '\r') {
-                jlex->col_no = 0;
-        } else if (jlex->c == '\t') {
-                jlex->col_no += JSON_TAB_WIDTH;
-        } else {
-                jlex->col_no++;
-        }
-}
 
 struct NXJSONLexer *nx_json_lexer_new(char *json_text)
 {
         struct NXJSONLexer *jlex = NX_NEW(1, struct NXJSONLexer);
-        jlex->s = json_text;
-        jlex->p = -1;
-        jlex->c = EOF;
-        jlex->line_no = 1;
-        jlex->col_no = 0;
-
-        nx_json_lexer_consume(jlex);
-
+        jlex->lex = nx_lexer_new(json_text);
         return jlex;
 }
 
 void nx_json_lexer_free(struct NXJSONLexer *jlex)
 {
         if (jlex != NULL) {
+                nx_lexer_free(jlex->lex);
                 nx_free(jlex);
         }
 }
@@ -168,151 +140,85 @@ static NXBool nx_json_lexer_is_whitespace(int c)
 
 static void nx_json_lexer_skip_whitespace(struct NXJSONLexer *jlex)
 {
-        while (nx_json_lexer_is_whitespace(jlex->c))
-                nx_json_lexer_consume(jlex);
+        while (nx_json_lexer_is_whitespace(nx_lexer_current_char(jlex->lex)))
+                nx_lexer_consume(jlex->lex);
 }
 
-static void nx_json_lexer_match(struct NXJSONLexer *jlex, const char *s)
+static inline void nx_json_lexer_match(struct NXJSONLexer *jlex, const char *s)
 {
-        int n = strlen(s);
-        while (n--) {
-                if (jlex->c == *s)
-                        nx_json_lexer_consume(jlex);
-                else
-                        NX_JSON_LEXER_ERROR(jlex,"Expecting '%c', found '%c'!",*s,jlex->c);
-                ++s;
-        }
+        if (!nx_lexer_match(jlex->lex, s))
+                NX_JSON_LEXER_ERROR(jlex,"Expecting '%s'!",s);
 }
 
 static void nx_json_lexer_TRUE(struct NXJSONLexer *jlex, struct NXJSONToken *t)
 {
-        nx_json_lexer_consume(jlex);
+        nx_lexer_consume(jlex->lex);
         nx_json_lexer_match(jlex, "rue");
         nx_json_token_fill(t, NX_JTT_TRUE, 0, NULL);
 }
 
 static void nx_json_lexer_FALSE(struct NXJSONLexer *jlex, struct NXJSONToken *t)
 {
-        nx_json_lexer_consume(jlex);
+        nx_lexer_consume(jlex->lex);
         nx_json_lexer_match(jlex, "alse");
         nx_json_token_fill(t, NX_JTT_FALSE, 0, NULL);
 }
 
 static void nx_json_lexer_NULL(struct NXJSONLexer *jlex, struct NXJSONToken *t)
 {
-        nx_json_lexer_consume(jlex);
+        nx_lexer_consume(jlex->lex);
         nx_json_lexer_match(jlex, "ull");
         nx_json_token_fill(t, NX_JTT_NULL, 0, NULL);
 }
 
 static void nx_json_lexer_NUMBER(struct NXJSONLexer *jlex, struct NXJSONToken *t)
 {
-        int start = jlex->p;
+        int len;
+        enum NXLexerNumberType type;
+        const char * res = nx_lexer_NUMBER(jlex->lex, &len, &type);
 
-        if (jlex->c == '-')
-                nx_json_lexer_consume(jlex);
-
-        while (isdigit(jlex->c))
-                nx_json_lexer_consume(jlex);
-
-        int end = jlex->p;
-        int len = end-start;
-
-        if (jlex->s[start] == '-' && len == 1)
-                NX_JSON_LEXER_ERROR(jlex,"%c should be followed by at least one digit", '-');
-        else if (jlex->s[start] == '0' && len > 1)
-                NX_JSON_LEXER_ERROR(jlex,"Multi-digit numbers can not start with %d", 0);
-        else if (len > 2 && jlex->s[start] == '-' && jlex->s[start+1] == '0')
-                NX_JSON_LEXER_ERROR(jlex,"Multi-digit numbers can not start with -%d", 0);
-
-        if (jlex->c != '.' && jlex->c != 'e' && jlex->c != 'E') {
-                nx_json_token_fill(t, NX_JTT_INTEGER, len, jlex->s + start);
-                return;
+        switch(type) {
+        case NX_LNT_INTEGER: nx_json_token_fill(t, NX_JTT_INTEGER, len, res); return;
+        case NX_LNT_FLOATING_POINT: nx_json_token_fill(t, NX_JTT_FPNUM, len, res); return;
+        default:
+                NX_JSON_LEXER_ERROR(jlex,"JSON lexer unknown number type: '%d'",(int)type);
         }
-
-        if (jlex->c == '.') {
-                nx_json_lexer_consume(jlex);
-                if (!isdigit(jlex->c))
-                        NX_JSON_LEXER_ERROR(jlex,"Floating-point fraction should have at least one digit%c", '!');
-                while (isdigit(jlex->c))
-                        nx_json_lexer_consume(jlex);
-        }
-
-        if (jlex->c == 'e' || jlex->c == 'E') {
-                nx_json_lexer_consume(jlex);
-                if (jlex->c == '+' || jlex->c == '-')
-                        nx_json_lexer_consume(jlex);
-                if (!isdigit(jlex->c))
-                        NX_JSON_LEXER_ERROR(jlex,"Floating-point exponent should have at least one digit%c", '!');
-                while (isdigit(jlex->c))
-                        nx_json_lexer_consume(jlex);
-        }
-
-        end = jlex->p;
-        len = end-start;
-        nx_json_token_fill(t, NX_JTT_FPNUM, len, jlex->s + start);
 }
 
 static void nx_json_lexer_STRING(struct NXJSONLexer *jlex, struct NXJSONToken *t)
 {
-        int start = jlex->p+1; // skip "
-        do {
-                if (jlex->c == '\\') {
-                        nx_json_lexer_consume(jlex);
-                        switch (jlex->c) {
-                        case '"':
-                        case '\\':
-                        case '/':
-                        case 'b':
-                        case 'f':
-                        case 'n':
-                        case 'r':
-                        case 't':
-                                nx_json_lexer_consume(jlex);
-                                break;
-                        default:
-                                NX_JSON_LEXER_ERROR(jlex,"Unknown control character in string: \\%c", jlex->c);
-                        }
-                } else {
-                        nx_json_lexer_consume(jlex);
-                }
-        } while (jlex->c != EOF && jlex->c != '"');
-
-        if (jlex->c != '"')
-                NX_JSON_LEXER_ERROR(jlex,"Missing closing %c for string", '\"');
-        nx_json_lexer_consume(jlex);
-
-        int end = jlex->p-1; // omit "
-        int len = end - start;
-        nx_json_token_fill_string(t, len, jlex->s + start);
+        int len;
+        const char * res = nx_lexer_QUOTED_STRING(jlex->lex, &len);
+        nx_json_token_fill_string(t, len, res);
 }
 
 void nx_json_lexer_next_token(struct NXJSONLexer *jlex, struct NXJSONToken *t)
 {
-        while (jlex->c != EOF) {
-                if (nx_json_lexer_is_whitespace(jlex->c)) {
+        while (nx_lexer_current_char(jlex->lex) != EOF) {
+                int c = nx_lexer_current_char(jlex->lex);
+                if (nx_json_lexer_is_whitespace(c)) {
                         nx_json_lexer_skip_whitespace(jlex);
                         continue;
                 }
 
-                switch (jlex->c) {
-                case '[': nx_json_lexer_consume(jlex); nx_json_token_fill(t, NX_JTT_LSBRAC, 0, NULL); return;
-                case ']': nx_json_lexer_consume(jlex); nx_json_token_fill(t, NX_JTT_RSBRAC, 0, NULL); return;
-                case '{': nx_json_lexer_consume(jlex); nx_json_token_fill(t, NX_JTT_LCBRAC, 0, NULL); return;
-                case '}': nx_json_lexer_consume(jlex); nx_json_token_fill(t, NX_JTT_RCBRAC, 0, NULL); return;
-                case ',': nx_json_lexer_consume(jlex); nx_json_token_fill(t, NX_JTT_COMMA, 0, NULL); return;
-                case ':': nx_json_lexer_consume(jlex); nx_json_token_fill(t, NX_JTT_COLON, 0, NULL); return;
+                switch (c) {
+                case '[': nx_lexer_consume(jlex->lex); nx_json_token_fill(t, NX_JTT_LSBRAC, 0, NULL); return;
+                case ']': nx_lexer_consume(jlex->lex); nx_json_token_fill(t, NX_JTT_RSBRAC, 0, NULL); return;
+                case '{': nx_lexer_consume(jlex->lex); nx_json_token_fill(t, NX_JTT_LCBRAC, 0, NULL); return;
+                case '}': nx_lexer_consume(jlex->lex); nx_json_token_fill(t, NX_JTT_RCBRAC, 0, NULL); return;
+                case ',': nx_lexer_consume(jlex->lex); nx_json_token_fill(t, NX_JTT_COMMA, 0, NULL); return;
+                case ':': nx_lexer_consume(jlex->lex); nx_json_token_fill(t, NX_JTT_COLON, 0, NULL); return;
                 case 't': ; nx_json_lexer_TRUE(jlex, t); return;
                 case 'f': ; nx_json_lexer_FALSE(jlex, t); return;
                 case 'n': ; nx_json_lexer_NULL(jlex, t); return;
                 case '"': ; nx_json_lexer_STRING(jlex, t); return;
                 default:
-                        if (jlex->c == '-' || isdigit(jlex->c)) {
+                        if (c == '-' || isdigit(c)) {
                                 nx_json_lexer_NUMBER(jlex, t);
                                 return;
                         }
 
-                        NX_JSON_LEXER_ERROR(jlex,"Unexpected character while parsing JSON: '%c'",jlex->c);
+                        NX_JSON_LEXER_ERROR(jlex,"Unexpected character while parsing JSON: '%c'",c);
                 }
         }
 
---
+++ tests/tests_json_lexer.cc
@@ -0,0 +1,181 @@
+/**
+ * @file tests_json_lexer.cc
+ *
+ * This file is part of the IYTE Visual Intelligence Research Group Software Library
+ *
+ * Copyright (C) 2015 Mustafa Ozuysal. All rights reserved.
+ *
+ * @author Mustafa Ozuysal
+ *
+ * Contact mustafaozuysal@iyte.edu.tr for comments and bug reports.
+ *
+ */
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+
+#include "gtest/gtest.h"
+
+#include "virg/nexus/nx_alloc.h"
+#include "virg/nexus/nx_json_lexer.h"
+#include "virg/nexus/nx_message.h"
+#include "virg/nexus/nx_string.h"
+
+using namespace std;
+
+namespace {
+
+class NXJSONLexerTest : public ::testing::Test {
+protected:
+        NXJSONLexerTest() {
+        }
+
+        virtual void SetUp() {
+        }
+
+        virtual void TearDown() {
+        }
+};
+
+TEST_F(NXJSONLexerTest, new_free) {
+        char *s = nx_strdup("{}");
+        struct NXJSONLexer *jlex = nx_json_lexer_new(s);
+        nx_json_lexer_free(jlex);
+        nx_free(s);
+}
+
+TEST_F(NXJSONLexerTest, empty_object) {
+        char *s = nx_strdup("{}");
+        struct NXJSONLexer *jlex = nx_json_lexer_new(s);
+
+        struct NXJSONToken t = { NX_JTT_INVALID, NULL};
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_LCBRAC, t.type);
+        EXPECT_EQ(NULL, t.text);
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_RCBRAC, t.type);
+        EXPECT_EQ(NULL, t.text);
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_EOF, t.type);
+        EXPECT_EQ(NULL, t.text);
+
+        nx_json_token_free_data(&t);
+        nx_json_lexer_free(jlex);
+        nx_free(s);
+}
+
+TEST_F(NXJSONLexerTest, empty_array) {
+        char *s = nx_strdup("[]");
+        struct NXJSONLexer *jlex = nx_json_lexer_new(s);
+
+        struct NXJSONToken t = { NX_JTT_INVALID, NULL};
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_LSBRAC, t.type);
+        EXPECT_EQ(NULL, t.text);
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_RSBRAC, t.type);
+        EXPECT_EQ(NULL, t.text);
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_EOF, t.type);
+        EXPECT_EQ(NULL, t.text);
+
+        nx_json_token_free_data(&t);
+        nx_json_lexer_free(jlex);
+        nx_free(s);
+}
+
+TEST_F(NXJSONLexerTest, object) {
+        char *s = nx_strdup("{ \"abc\" : 123 }");
+        struct NXJSONLexer *jlex = nx_json_lexer_new(s);
+
+        struct NXJSONToken t = { NX_JTT_INVALID, NULL};
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_LCBRAC, t.type);
+        EXPECT_EQ(NULL, t.text);
+
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_STRING, t.type);
+        EXPECT_EQ(0, strcmp(t.text, "abc"));
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_COLON, t.type);
+        EXPECT_EQ(NULL, t.text);
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_INTEGER, t.type);
+        EXPECT_EQ(0, strcmp(t.text, "123"));
+
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_RCBRAC, t.type);
+        EXPECT_EQ(NULL, t.text);
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_EOF, t.type);
+        EXPECT_EQ(NULL, t.text);
+
+        nx_json_token_free_data(&t);
+        nx_json_lexer_free(jlex);
+        nx_free(s);
+}
+
+TEST_F(NXJSONLexerTest, array) {
+        char *s = nx_strdup("[ \"abc\", 123, 123.123, true, false, null ]");
+        struct NXJSONLexer *jlex = nx_json_lexer_new(s);
+
+        struct NXJSONToken t = { NX_JTT_INVALID, NULL};
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_LSBRAC, t.type);
+        EXPECT_EQ(NULL, t.text);
+
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_STRING, t.type);
+        EXPECT_EQ(0, strcmp(t.text, "abc"));
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_COMMA, t.type);
+        EXPECT_EQ(NULL, t.text);
+
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_INTEGER, t.type);
+        EXPECT_EQ(0, strcmp(t.text, "123"));
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_COMMA, t.type);
+        EXPECT_EQ(NULL, t.text);
+
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_FPNUM, t.type);
+        EXPECT_EQ(0, strcmp(t.text, "123.123"));
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_COMMA, t.type);
+        EXPECT_EQ(NULL, t.text);
+
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_TRUE, t.type);
+        EXPECT_EQ(NULL, t.text);
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_COMMA, t.type);
+        EXPECT_EQ(NULL, t.text);
+
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_FALSE, t.type);
+        EXPECT_EQ(NULL, t.text);
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_COMMA, t.type);
+        EXPECT_EQ(NULL, t.text);
+
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_NULL, t.type);
+        EXPECT_EQ(NULL, t.text);
+
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_RSBRAC, t.type);
+        EXPECT_EQ(NULL, t.text);
+        nx_json_lexer_next_token(jlex, &t);
+        EXPECT_EQ(NX_JTT_EOF, t.type);
+        EXPECT_EQ(NULL, t.text);
+
+        nx_json_token_free_data(&t);
+        nx_json_lexer_free(jlex);
+        nx_free(s);
+}
+
+
+} // namespace
