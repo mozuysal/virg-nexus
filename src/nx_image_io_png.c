b4b3f116bc01f853bf78939d054ce21c874ec50c
Mustafa Ozuysal <mustafaozuysal@iyte.edu.tr>
2019-03-08 15:09:29 +0300

    initial commit for PNG read support

--- src/nx_image_io_png.c
+++ src/nx_image_io_png.c
@@ -23,9 +23,7 @@
 #include "virg/nexus/nx_log.h"
 #include "virg/nexus/nx_io.h"
 
-
-
-NXResult save_as_png(const struct NXImage *img, FILE *fout)
+static NXResult save_as_png(const struct NXImage *img, FILE *fout)
 {
         NX_ASSERT_PTR(img);
         NX_IMAGE_ASSERT_UCHAR(img);
@@ -94,6 +92,104 @@ NXResult save_as_png(const struct NXImage *img, FILE *fout)
         return NX_OK;
 }
 
+static NXResult load_as_png(struct NXImage *img, FILE *fin,
+                            enum NXImageLoadMode mode)
+{
+        NX_ASSERT_PTR(img);
+        NX_ASSERT_PTR(fin);
+        NX_IMAGE_ASSERT_UCHAR(img);
+
+        png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,
+                                                     NULL, NULL, NULL);
+        if (!png_ptr) {
+                NX_ERROR(NX_LOG_TAG, "Could not create PNG read structure");
+                return NX_FAIL;
+        }
+
+        png_infop info_ptr = png_create_info_struct(png_ptr);
+        if (!info_ptr) {
+                NX_ERROR(NX_LOG_TAG, "Could not create PNG info pointer");
+                png_destroy_read_struct(&png_ptr, png_infopp_NULL,
+                                        png_infopp_NULL);
+                return NX_FAIL;
+        }
+
+        if (setjmp(png_jmpbuf(png_ptr))) {
+                NX_ERROR(NX_LOG_TAG, "Could not set jump point for reading PNG file");
+                png_destroy_read_struct(&png_ptr, &info_ptr, png_infopp_NULL);
+                return NX_FAIL;
+        }
+
+        png_init_io(png_ptr, fin);
+        png_read_info(png_ptr, info_ptr);
+
+        png_uint_32 stream_width, stream_height;
+        int bit_depth, color_type, interlace_type;
+        png_get_IHDR(png_ptr, info_ptr, &stream_width, &stream_height, &bit_depth, &color_type,
+                     &interlace_type, int_p_NULL, int_p_NULL);
+
+        png_set_strip_16(png_ptr);
+        if (color_type == PNG_COLOR_TYPE_GA) {
+                png_set_strip_alpha(png_ptr); /*(not recommended). */
+        }
+
+        png_set_packing(png_ptr);
+        if (color_type == PNG_COLOR_TYPE_PALETTE) {
+                png_set_palette_to_rgb(png_ptr);
+        }
+
+        if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8) {
+                png_set_gray_1_2_4_to_8(png_ptr);
+        }
+
+        enum NXImageType output_type = NX_IMAGE_RGBA;
+        if (mode == NX_IMAGE_LOAD_GRAYSCALE) {
+                output_type = NX_IMAGE_GRAYSCALE;
+                png_set_rgb_to_gray_fixed(png_ptr, 1, 30000, 59000);
+                png_set_strip_alpha(png_ptr); /*(not recommended). */
+        } else if (mode == NX_IMAGE_LOAD_RGBA) {
+                output_type = NX_IMAGE_RGBA;
+
+                if (color_type == PNG_COLOR_TYPE_GRAY ||
+                    color_type == PNG_COLOR_TYPE_GA) {
+                        png_set_gray_to_rgb(png_ptr);
+                        png_set_add_alpha(png_ptr, 255, PNG_FILLER_AFTER);
+                }
+        } else {
+                if (color_type == PNG_COLOR_TYPE_GRAY ||
+                    color_type == PNG_COLOR_TYPE_GA) {
+                        output_type = NX_IMAGE_GRAYSCALE;
+                }
+
+                if(color_type == PNG_COLOR_TYPE_RGB ||
+                   color_type == PNG_COLOR_TYPE_PALETTE) {
+                        png_set_add_alpha(png_ptr, 255, PNG_FILLER_AFTER);
+                }
+        }
+
+        if (stream_width  != (unsigned int)img->width ||
+            stream_height != (unsigned int)img->height ||
+            output_type   != img->type) {
+                nx_image_resize(img, stream_width, stream_height,
+                                NX_IMAGE_STRIDE_DEFAULT, output_type,
+                                NX_IMAGE_UCHAR);
+        }
+
+        png_bytepp row_pointers = NX_NEW(img->height, png_bytep);
+        for (int row = 0; row < img->height; row++) {
+                row_pointers[row] = img->data.uc + row * img->row_stride;
+        }
+
+        png_read_image(png_ptr, row_pointers);
+        png_read_end(png_ptr, info_ptr);
+
+        png_destroy_read_struct(&png_ptr, &info_ptr, png_infopp_NULL);
+
+        nx_free(row_pointers);
+
+        return NX_OK;
+}
+
 void nx_image_xsave_png(const struct NXImage *img, const char *filename)
 {
         NX_ASSERT_PTR(img);
@@ -111,7 +207,14 @@ void nx_image_xsave_png(const struct NXImage *img, const char *filename)
 
 void nx_image_xload_png(struct NXImage *img, const char *filename, enum NXImageLoadMode mode)
 {
-        NX_FATAL(NX_LOG_TAG, "PNG Loading not supported");
+        NX_ASSERT_PTR(img);
+        NX_ASSERT_PTR(filename);
+        NX_ASSERT_CUSTOM("Only UCHAR images can be loaded directly from PNG files", img->dtype==NX_IMAGE_UCHAR);
+
+        FILE *png_stream = nx_xfopen(filename, "rb");
+        if (load_as_png(img, png_stream, mode) != NX_OK)
+                NX_FATAL(NX_LOG_TAG, "Error loading PNG stream from file %s!", filename);
+        nx_xfclose(png_stream, filename);
 }
 
 NXResult nx_image_save_png(const struct NXImage *img, const char *filename)
@@ -137,6 +240,23 @@ NXResult nx_image_save_png(const struct NXImage *img, const char *filename)
 
 NXResult nx_image_load_png(struct NXImage *img, const char *filename, enum NXImageLoadMode mode)
 {
-        return NX_FAIL;
+        NX_ASSERT_PTR(img);
+        NX_ASSERT_PTR(filename);
+        NX_ASSERT_CUSTOM("Only UCHAR images can be loaded directly from PNG files", img->dtype==NX_IMAGE_UCHAR);
+
+        FILE *png_stream = nx_fopen(filename, "rb");
+        if (!png_stream) {
+                return NX_FAIL;
+        }
+
+        NXResult res_read = load_as_png(img, png_stream, mode);
+        if (res_read != NX_OK)
+                NX_ERROR(NX_LOG_TAG, "Error loading PNG stream from file %s!", filename);
+        NXResult res_close = nx_fclose(png_stream, filename);
+
+        if (res_read == NX_OK)
+                return res_close;
+        else
+                return res_read;
 }
 
