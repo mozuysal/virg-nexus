7d2b42ea03d8ffc80f104e554453fa3b49249783
Mustafa Ozuysal <mustafaozuysal@iyte.edu.tr>
2019-10-12 11:42:27 +0300

    Convinience function to create OpenGL programs from vertex/fragment shaders sources

--- include/virg/nexus/GL/nx_gl_program.h
+++ include/virg/nexus/GL/nx_gl_program.h
@@ -28,11 +28,30 @@
 
 #include "virg/nexus/nx_config.h"
 #include "virg/nexus/GL/nx_gl.h"
+#include "virg/nexus/GL/nx_gl_shader.h"
 
 __NX_BEGIN_DECL
 
 GLuint nx_gl_program_xcreate_and_link(GLuint v_shader, GLuint f_shader);
 void   nx_gl_program_xvalidate(GLuint program);
+
+static inline GLuint nx_gl_program_from_vf_shaders(const char *v_shader_src,
+                                                   const char *f_shader_src)
+{
+        GLuint v_shader = nx_gl_shader_xcreate_and_compile(GL_VERTEX_SHADER,
+                                                           v_shader_src);
+        GLuint f_shader = nx_gl_shader_xcreate_and_compile(GL_FRAGMENT_SHADER,
+                                                           f_shader_src);
+
+        GLuint program = nx_gl_program_xcreate_and_link(v_shader, f_shader);
+        nx_gl_program_xvalidate(program);
+
+        glDeleteShader(v_shader);
+        glDeleteShader(f_shader);
+
+        return program;
+}
+
 
 __NX_END_DECL
 
